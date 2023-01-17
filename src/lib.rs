c0ba81cba5b8456d586cf70bb4d6ff8dacf06fc3
AdamWewe <adammehdiarafan@gmail.com>
2023-01-17 01:13:48 +0100

    added backward + started implementing topological search

--- src/lib.rs
+++ src/lib.rs
@@ -4,61 +4,100 @@ use petgraph::dot::Dot;
 use std::convert::From;
 use std::ops::Add;
 use std::ops::Mul;
+use std::collections::HashSet;
+use std::cmp::{Eq, PartialEq};
 
-#[derive(Debug, Clone, Default)]
-pub struct Value <T>
+
+
+#[derive(Clone, Debug, Default)]
+pub struct Value
+    // where F: Fn(usize) -> usize
 {
     pub result : f64,  // replace with vec to keep track of changes
     pub children : Vec<f64>,
-    pub op : Vec<char>,  
+    pub op : Vec<String>,  
     pub grad : f64,
     pub label : Vec<String>,
-    pub backward : T
+    pub _backward : () //Box<dyn FnOnce()>
 }
 
-impl <T: std::default::Default + std::fmt::Debug> Value <T>{
-    pub fn new(input: f64, lab: String) -> Self {
+// impl <F: std::default::Default + std::fmt::Debug + Copy> Value <F> 
+    // where F: std::ops::Fn(usize) -> usize {
+    impl Value {
+        pub fn new(input: f64, lab: String, gradient: Option<f64>) -> Self {
+                
         Self{result : input,
             children : Vec::new(),
             op : Vec::new(),
-            grad : 0.0,
+            grad : match gradient {
+                Some(x) => x,
+                None => 0.0
+            },
             label: vec![lab],
-            backward : Default::default()
+            _backward : Default::default()
         }
-    }
+    }  
 
     pub fn repr(&self) -> String {
         format!("Value(result={:?})", self)
     }
 
-    pub fn add(&mut self, other: f64) {
+    pub fn add(&mut self, other: & mut Value) //<F>)//, func: &dyn Fn()) 
+        // where F: std::ops::Fn(usize) -> usize 
+    {
         self.children.push(self.result);
-        self.children.push(other);
-        self.result = self.result + other;
+        self.children.push(other.result);
+        self.result = self.result + other.result;
 
         // verbosity to be fixed:
+        self.op.push(String::from("+")); 
 
-        self.op.push('+');
+        let mut f = (|| {
+        self.grad = 1.0 * self.grad;
+        other.grad = 1.0 * self.grad;
+        })();  // test this further
+        
+        // self._backward = f;
 
+
+        // let _f = (|| {
+        //     println!("inside!")
+        // })();
     }
 
-    pub fn mul(& mut self, other: f64) {
+    pub fn mul(& mut self, other: &mut Value) { //<F>)
+        // where F: std::ops::Fn(usize) //usize {
         // self.prev.push(self.result);
         self.children.push(self.result);
-        self.children.push(other);
+        self.children.push(other.result);
 
-        self.result = self.result * other;  // reassignment
+        self.result = self.result * other.result;  // reassignment
 
-        self.op.push('*'); // for sign 
+        self.op.push(String::from("*")); // for sign 
+
+        // let mut f = (|| {
+        self.grad = self.result * self.grad;
+        other.grad = self.result * self.grad;
+            // })();  // test this further
+        println!("{:?}, {:?}", self.grad, self.result);    
+        // self._backward = f;
 
     }
 
-    pub fn grad(&mut self)
-//     where T: std::ops::Mul<Output=T> + std::ops::MulAssign + Copy       
-    {
+    pub fn grader(&mut self) {
 
         self.grad += 1.0 * f64::from(self.result);
     }
+
+
+    // todo: functional implmentation of backprop instead of locally scoping
+    // pub fn backward(&mut self, func: impl Fn(usize) -> usize + 'static){
+
+    //     // self.grad = 1.0 * self 
+
+    // }
+
+
     pub fn tanh(&mut self) -> Self 
     {
         let t = ((2.0 * self.result).exp() - 1.0) / 
@@ -66,35 +105,55 @@ impl <T: std::default::Default + std::fmt::Debug> Value <T>{
 
         // let formatter = ;
         self.label.push(String::from("tanh"));
+        self.op.push(String::from("tanh"));
+
+        let mut f = (|| {
+            self.grad = (1.0 - f64::powf(t, 2.0)) * self.grad;
+            })();
         
         Self{result : t,
             children : self.children.clone(),
             op : self.op.clone(),
             grad : self.grad,
             label: self.label.clone(),
-            backward : Default::default()
+            _backward : self._backward
         }
+    }
+
+    pub fn backward(self) {
+        
+        let topo: Vec<f64> = Vec::new();
+        // let visited : HashSet<f64> = HashSet(2);
+
+        // let topo_builder = (|x|{
+            // if !visited.contains(&x) {
+
+            // }
+        // }
+        // )(self)
+
+
     }
 }
 
-impl <T: std::default::Default> Add for Value<T> {
+// impl <F: std::default::Default + std::ops::Fn(usize) -> usize> Add for Value<F> {
+impl Add for Value {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self
-    {
+    fn add(self, other: Self) -> Self {
         Self {
             result: self.result + other.result,
             children : self.children,
             op : self.op,
             grad : self.grad,
             label: self.label,
-            backward : Default::default()
+            _backward : self._backward
         }
     }
-
 }
 
-impl <T: std::default::Default> Mul for Value <T> {
+// impl <F: std::default::Default + std::ops::Fn(usize) -> usize> Mul for Value<F> {
+    impl Mul for Value {
     type Output = Self;
 
     fn mul(self, other: Self) -> Self
@@ -105,11 +164,18 @@ impl <T: std::default::Default> Mul for Value <T> {
             op : self.op,
             grad : self.grad,
             label: self.label,
-            backward : Default::default()
+            _backward : self._backward
         }
     }
 }
 
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        self.result == other.result
+    }
+}
+
+impl Eq for Value {}
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,32 @@ use lib::Value;
 
 fn main() {
 
-    let mut variable : Value<f64> = Value::new(32.0, "a".into());
+    let mut variable : Value = Value::new(32.0, "a".into(), Some(1.0));
 
 
-    let mut vals : Value<f64> = Value::new(32.0, "a".into()); 
-    vals.add(4.0);
-    vals.mul(2.0);
-    // println!("{:?}", vals);
+    let mut vals : Value = Value::new(32.0, "a".into(), Some(2.0)); 
+    vals.add(&mut Value::new(4.0, "b".into(), Some(4.0)));
+    vals.mul(&mut Value::new(4.0, "c".into(), Some(5.0)));
+    println!("{:?}", vals);
 
-    vals.add(1.0);
-    println!("{:?}", vals.repr());
+    // vals.add(Value::new(45.0, "d".into()));
+    // println!("{:?}", vals.repr());
 
-    vals.add(2.0);
-    println!("{:?}", vals.repr());
+    // vals.mul(Value::new(2.0, "e".into()));
+    // println!("{:?}", vals.repr());
 
-    vals.add(4.9);
+    // vals.add(Value::new(1.0, "f".into()));
 
 
-    println!("{:?}", vals.repr());
+    // println!("{:?}", vals.repr());
 
-    let mut vals2 :  Value<f64> = Value::new(20.0, "b".into());  
-    println!("{:?}", vals.repr());
-    vals.tanh();
+    // let mut vals2 :  Value<f64> = Value::new(20.0, "b".into());  
+    // println!("{:?}", vals.repr());
+    // vals.tanh();
 
-    println!("{:?}", vals.repr());
+    // println!("{:?}", vals.repr());
 
-    vals2.tanh();
-    println!("{:?}", vals2.repr());
+    // vals2.tanh();
+    // println!("{:?}", vals2.repr());
     // vals2.visualize();
 }
